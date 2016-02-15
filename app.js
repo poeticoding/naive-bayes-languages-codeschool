97acd2207dbe3a75bfe50a24db59e11f1f50de7e
Alvise Susmel <alvise@poeticoding.com>
2016-02-15 12:55:14 +0000

    italian training phrases, npm run classify

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 "use strict";
 
+let phrase = process.argv[2];
+if(!phrase) {
+	console.error("Error, you need to specify a phrase to classify")
+	return -1
+}
+
 let LanguageClassifier = require('./language_classifier');
 
 let clf = new LanguageClassifier();
 
 clf.trainLanguages().then(()=>{
-
-	let phrase = "Hello man, Où peut-on obtenir des billets?";
 	let label = clf.classify(phrase);
 	console.log(`${phrase} --> ${label}`);
 
